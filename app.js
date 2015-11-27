f9f22dc8a365b9206bb0540f3437b5a6b543d459
click20 <rocco.davide@icloud.com>
2015-11-27 13:18:16 +0100

    esercizio completo

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 /* your code should go here */
 $(document).ready(function(){
- 
-    
+    $("#btn-filter").click( function(){
+        $("#summary").html("");
+        controller.init( $("select[id=cities]").val() );
+    })
+       
 });
 
 
@@ -22,111 +25,69 @@ var model = {
         return week;     
     },
     
-    cityMinMaxTemp: function(city){
+    cityMinMaxTemp: function(city,day){
         var trial= []; var arrayTemp=[]; var minTemp=0; var maxTemp=0;
             for(var i=0; i<this.collection.length; i++){
-                if( this.collection[i].city == city ){
-                    trial.push({
-                        day: this.collection[i].day
-                    });           
+                if( this.collection[i].city == city && this.collection[i].day == day ){    
+                    trial.push(this.collection[i].temperature);           
+            }else{
+                continue;
             }
+        }
         minTemp= Math.min(...trial);
         maxTemp= Math.max(...trial);
         arrayTemp.push(minTemp,maxTemp);
 //        console.log(arrayTemp);
-        
-        }
-        return trial;
+           
+        return arrayTemp;
     }
 };
 
-var view={
-    
+var view = {
+       
+        init: function(city) {
+            
+            var tmpl = '<li> <div class="icon"> <img src="img/icons/SOST.png"> </div> <div class="stats"> <h2>DAY</h2> <strong>min</strong> MINºC <strong>max</strong> MAXºC </div> </li>';
+            for(var f=0; f<4; f++){
+                    $("#summary").append(                                                                                                          tmpl.replace("SOST",controller.getCondition(f)).replace("DAY",controller.getDay(f)).replace("MIN",controller.getMinTemp(city,controller.getWeek(f).day)).replace("MAX",controller.getMaxTemp(city,controller.getWeek(f).day)) );
+            }
+        }
+ 
 };
 
-var controller={
-    
-};
 
-//
-//
-//var model={
-//    
-//    init: function(pos){
-//        var week=[];
-//        
-//        for(var l=0; l<data.length; l++){
-//        week.push({
-//            day: data[l].day,
-//            condition: data[l].condition
-//        }); 
-//        l=l+3;
-//        }    
-//        return week;
-//    },
-//    
-//    MinMaxTemp: function(day){
-//        var trial= []; var arrayTemp=[]; var minTemp=0; var maxTemp=0;
-//            for(var i=0; i<data.length; i++){
-//                if( data[i].day == day ){
-//                    trial.push(data[i].temperature);
-////                        console.log(trial);
-//                    }           
-//            }
-//        minTemp= Math.min(...trial);
-//        maxTemp= Math.max(...trial);
-//        arrayTemp.push(minTemp,maxTemp);
-////        console.log(arrayTemp);
-//        return arrayTemp;
-//    }
-// 
-//};
-//
-//var view = {
-//       
-//        init: function() {
-//            
-//            var tmpl = '<li> <div class="icon"> <img src="img/icons/SOST.png"> </div> <div class="stats"> <h2>DAY</h2> <strong>min</strong> MINºC <strong>max</strong> MAXºC </div> </li>';
-//            for(var f=0; f<4; f++){
-//$("#summary").append(tmpl.replace("SOST",controller.getCondition(f)).replace("DAY",controller.getDay(f)).replace("MIN",controller.getMinTemp(controller.getWeek(f).day)).replace("MAX",controller.getMaxTemp(controller.getWeek(f).day)) );
-//            }
-//        }
-// 
-//};
-//    
-//var controller= {
-//            
-//            init: function() {
-//                view.init();
-//                
-//            },
-//            
-//            getWeek: function(position){
-//                var WEEK= model.init(position);
-//                return WEEK[position];
-//            },
-//    
-//            getDay: function(position){
-//                var DAY= model.init(position);
-//                return DAY[position].day;
-//            },
-//    
-//            getCondition: function(position){
-//                var CONDITION= model.init(position);
-//                return CONDITION[position].condition;
-//            },
-//    
-//            getMinTemp: function(day){
-//                var MIN= model.MinMaxTemp(day);
-//                return MIN[0];
-//            },
-//            
-//            getMaxTemp: function(day){
-//                var MAX= model.MinMaxTemp(day);
-//                return MAX[1];
-//            },
-//            
-//};
+var controller= {
+            
+            init: function(city) {
+                view.init(city);      
+            },
+            
+            getWeek: function(position){
+                var WEEK= model.init(position);
+                return WEEK[position];
+            },
+    
+            getDay: function(position){
+                var DAY= model.init(position);
+                return DAY[position].day;
+            },
+    
+            getCondition: function(position){
+                var CONDITION= model.init(position);
+                return CONDITION[position].condition;
+            },
+    
+            getMinTemp: function(city,day){
+                var MIN= model.cityMinMaxTemp(city,day);
+                return MIN[0];
+            },
+            
+            getMaxTemp: function(city,day){
+                var MAX= model.cityMinMaxTemp(city,day);
+                return MAX[1];
+            },
+            
+};
 
 
 
